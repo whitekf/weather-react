a0fdda10392f7fcc73800aec3a2e67f7f7c35fc2
whitekf <90643199+whitekf@users.noreply.github.com>
2022-08-04 19:08:17 -0400

    Working on adding loader spinner

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { Audio } from "react-loader-spinner";
 
 export default function Weather(props) {
   let city = "Tokyo";
@@ -13,5 +14,18 @@ export default function Weather(props) {
   let units = "metric";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from my Weather file! {city} </h2>;
+  return (
+    (<h2>Hello from my Weather file! {city} </h2>),
+    (
+      <Audio
+        height="80"
+        width="80"
+        radius="9"
+        color="green"
+        ariaLabel="three-dots-loading"
+        wrapperStyle
+        wrapperClass
+      />
+    )
+  );
 }
