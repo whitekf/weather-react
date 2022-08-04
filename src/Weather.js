116fb11899122f82e7c823273e604eed8d5a4c39
whitekf <90643199+whitekf@users.noreply.github.com>
2022-08-04 19:21:45 -0400

    Added multiple spinner loaders

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,13 @@
 import React from "react";
 import axios from "axios";
 import { Audio } from "react-loader-spinner";
+import { Rings } from "react-loader-spinner";
+import { TailSpin } from "react-loader-spinner";
+import { ThreeDots } from "react-loader-spinner";
+import { BallTriangle } from "react-loader-spinner";
+import { Hearts } from "react-loader-spinner";
+import { Oval } from "react-loader-spinner";
+import { Grid } from "react-loader-spinner";
 
 export default function Weather(props) {
   let city = "Tokyo";
@@ -15,17 +22,24 @@ export default function Weather(props) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(handleResponse);
   return (
-    (<h2>Hello from my Weather file! {city} </h2>),
-    (
+    <div>
+      <h2>Hello from my Weather file! {city} </h2>
       <Audio
         height="80"
         width="80"
         radius="9"
-        color="green"
+        color="blueviolet"
         ariaLabel="three-dots-loading"
         wrapperStyle
         wrapperClass
       />
-    )
+      <Hearts color="pink" height={80} width={80} />
+      <BallTriangle color="white" height={80} width={80} />
+      <Rings color="#00BFFF" height={80} width={80} />
+      <TailSpin color="red" height={80} width={80} />
+      <ThreeDots color="green" height={80} width={80} />
+      <Oval color="orange" height={80} width={80} />
+      <Grid color="yellow" height={80} width={80} />
+    </div>
   );
 }
